19b4be2341b0ab149bf4f0f829d6ff0a236fe953
Tu12581 <372286774@qq.com>
2025-02-26 16:11:14 +0800

    添加上下文处理器用于向前端自动传递usr_id等

---
+++ app/context_processors.py
@@ -0,0 +1,12 @@
+# 上下文处理器，用于避免每次都需要将userid传递到前端
+# 处理器需要添加到setting的TEMPLATE的context_processors中
+# context_processors.py
+def user_info(request):
+    # 如果用户已登录，返回用户信息
+    if request.user.is_authenticated:
+        return {
+            'user_id': request.user.user_id,
+            'user_name': request.user.user_name,
+        }
+    # 如果用户未登录，返回空字典
+    return {}
